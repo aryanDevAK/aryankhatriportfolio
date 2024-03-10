11d6a395ef7aa7f04b67ff70bb00255c5f6700cc
AK-Developers <aryankhatri9811@gmail.com>
2024-03-10 18:27:53 +0530

    portfolio

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
   
+
+const texts = ["Ecosystems & Systems Engineer", "Software Engineer", "Android Developer", "UI/UX Designer"];
+let index = 0;
+let text = "";
+let isTyping = true;
+
+function type() {
+  text = texts[index].substring(0, text.length + 1);
+  document.getElementById("changingText").textContent = text;
+
+  if (text === texts[index]) {
+    isTyping = false;
+    setTimeout(erase, 2000); // Wait 2 seconds before erasing the text
+  } else {
+    setTimeout(type, 200); // Typing speed: 200 milliseconds
+  }
+}
+
+function erase() {
+  text = texts[index].substring(0, text.length - 1);
+  document.getElementById("changingText").textContent = text;
+
+  if (text === "") {
+    isTyping = true;
+    index = (index + 1) % texts.length; // Cycle through the array of texts
+    setTimeout(type, 500); // Wait 0.5 seconds before typing the next text
+  } else {
+    setTimeout(erase, 100); // Erasing speed: 100 milliseconds
+  }
+}
+
+// Start the typing animation
+type();
