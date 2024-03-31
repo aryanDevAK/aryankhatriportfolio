2795c7bfa11f9d14cf3c9f48e20ee207d4aa8245
AK-Developers <aryankhatri9811@gmail.com>
2024-03-31 16:12:19 +0530

    portfolio

--- script.js
+++ script.js
@@ -44,3 +44,68 @@ darkModeButton.addEventListener("click", () => {
   body.style.backgroundColor = "#000";
   body.style.color = "#fff";
 });
+
+// Projects
+const projects = [
+  {
+    title: "Hospital Management System",
+    description: "<b>Tech Stack</b><br><li>Python</li><li>React</li><li>MySQL</li><br>An Ecosystem for hospital that enables strategical decision making driven by AI and technique of OCR that enables hospitals to go paper free.<br><b>Strenghts</b><li>The HIMS is able to reduce the paper consumption by 80% within the hospitals.</li><li>The ecosystem is also efficient for small scale clinics and can be customised as per the requirements. Upto 70% effective.</li><li>Data of Hospitals, Lab Reports, Patients, Prescription can be synchorinzed in real time.</li><li>The Management system can be implemented for all the government hospitals around the country.</li>",
+    imgUrl: "./assets/project-1.png",
+    gitHubLink: "",
+    liveDemoLink: "Under Development",
+    btnContent:"Under Development",
+    tab: "first",
+  },
+  {
+    title: "Kerala Lottery  App",
+    description: "Description 2",
+    imgUrl: "./assets/education.png",
+    gitHubLink: "",
+    liveDemoLink: "https://youtu.be/PYg21TJ9uyo",
+    btnContent:"View Demo" ,
+    tab: "second",
+  },
+  {
+    title: "My Designs",
+    description: "",
+    imgUrl: "./assets/education.png",
+    gitHubLink: "",
+    liveDemoLink: "https://youtu.be/PYg21TJ9uyo",
+    btnContent:"View Graphic Designs",
+    tab: "third",
+  },
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  showProjects("first");
+});
+
+function showProjects(tab) {
+  const projectContainer = document.getElementById("project-container");
+  projectContainer.innerHTML = "";
+
+  const filteredProjects = projects.filter(
+    (project) => project.tab === tab
+  );
+
+  filteredProjects.forEach((project) => {
+    const projectCard = document.createElement("div");
+    projectCard.classList.add("project-card");
+    projectCard.innerHTML = `
+    <div class="project-sub-container">
+      <div class="project-image">
+        <img src="${project.imgUrl}" alt="Project Image">
+      </div>
+      <div class="project-info">
+        <h2>${project.title}</h2>
+        <p>${project.description}</p>
+        <div class="project-btns">
+          <a href="${project.gitHubLink}"><button class="btn btn-color-2">Github &#128279;</button></a>
+          <a href="${project.liveDemoLink}"><button class="btn btn-color-1" id="project-btn">${project.btnContent} &#128279;</button></a>
+        </div>
+      </div>
+    </div>
+`;
+    projectContainer.appendChild(projectCard);
+  });
+}
